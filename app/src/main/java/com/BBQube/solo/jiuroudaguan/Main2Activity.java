958f8f949a9631420dcfdd9d6c0ff1825ba3072a
Ian-Cheng-Liu <lch.ian12@gmail.com>
2016-03-11 10:43:15 -0500

    latest version before deleting Main2Activity

--- app/src/main/java/com/BBQube/solo/jiuroudaguan/Main2Activity.java
+++ app/src/main/java/com/BBQube/solo/jiuroudaguan/Main2Activity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 
@@ -23,10 +24,13 @@ import java.util.ArrayList;
 
 public class Main2Activity extends AppCompatActivity {
 
+    private static final String TAG = "Main2Activity";
+
     private LineChart mChart;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "Main2Activity is Called");
         setContentView(R.layout.activity_main2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
